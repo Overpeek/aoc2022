e39d9edde35e51718ec79add2eeb1a8154855b8b
Eemeli Lehtonen <eemeli.o.lehtonen@utu.fi>
2022-12-05 19:17:53 +0200

    unnecessarily fast day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,44 +1,36 @@
-use std::collections::{HashMap, HashSet};
-
-//
-
 fn main(input: &str) -> impl crate::Results {
     let iter = input.lines().map(|s| s.as_bytes());
 
-    let t = std::time::Instant::now();
     let p1 = iter
         .clone()
         .map(|s| s.split_at(s.len() / 2))
         .flat_map(|(a, b)| b.iter().copied().find(move |c| a.contains(c)))
         .map(priority)
         .sum::<u32>();
-    println!("{:?}", t.elapsed());
 
-    let t = std::time::Instant::now();
     let p2 = iter
         .array_chunks::<3>()
-        .flat_map(|s| {
-            let mut map = HashMap::<_, (u8, usize)>::new();
+        .map(|s| {
+            let mut map = [(0, 0); 256];
             for (i, s) in s
                 .iter()
                 .enumerate()
                 .flat_map(|(i, s)| s.into_iter().map(move |c| (i, *c)))
             {
-                let (entry, already_in) = map.entry(s).or_default();
+                let (entry, already_in) = &mut map[s as usize];
 
                 if *already_in & (1 << i) == 0 {
+                    if *entry == 2 {
+                        return s;
+                    }
                     *already_in |= 1 << i;
                     *entry += 1;
-                    if *entry == 3 {
-                        return Some(s);
-                    }
                 }
             }
-            map.into_iter().find(|(_, (v, _))| *v == 3).map(|(k, _)| k)
+            unreachable!()
         })
         .map(priority)
         .sum::<u32>();
-    println!("{:?}", t.elapsed());
 
     (p1, p2)
 }
