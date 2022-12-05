b448ea31d63507fa6ba9542929e6c60d417c5e8b
Eemeli Lehtonen <eemeli.o.lehtonen@utu.fi>
2022-12-05 20:02:37 +0200

    improved codegen

--- gen.sh
+++ gen.sh
@@ -12,5 +12,5 @@ crate::bp!($day);" > "src/day$day.rs"
 touch "src/day$day.txt"
 touch "src/day${day}_test.txt"
 
-#sed -i 's/\\\ gen\ mod/mod day'$day';\n\\\ gen\ mod/g' "src/main.rs"
-#sed -i 's/\\\ gen\ run/day'$day'::run();\n\\\ gen\ run/g' "src/main.rs"
+sed -i 's/\/\/ gen mod/mod day'$day';\n\/\/ gen mod/g' "src/main.rs"
+sed -i 's/\/\/ gen run/day'$day'::run();\n    \/\/ gen run/g' "src/main.rs"
