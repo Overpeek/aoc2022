a8116043fe37b20df5602fd580791d40bb6d9e41
Overpeek <overpeek.fin@gmail.com>
2022-12-07 23:45:09 +0200

    parallel exec

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@
 use std::{
     env::args,
     fmt::Display,
-    sync::atomic::{AtomicBool, Ordering},
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
     time::Duration,
 };
+
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 //
 
@@ -24,6 +26,7 @@ mod day7;
 //
 
 static TEST_MODE: AtomicBool = AtomicBool::new(false);
+static BATCH_COUNT: AtomicUsize = AtomicUsize::new(1);
 
 //
 
@@ -31,15 +34,32 @@ fn main() {
     if args().any(|s| s == "-t") {
         TEST_MODE.store(true, Ordering::SeqCst);
     }
+    if let Some(n) = args().find(|s| s.starts_with("-n")) {
+        BATCH_COUNT.store(
+            n[2..]
+                .parse()
+                .expect("Failed to parse the batch count number"),
+            Ordering::SeqCst,
+        );
+    }
 
     let start = std::time::Instant::now();
-    day1::run();
-    day2::run();
-    day3::run();
-    day4::run();
-    day5::run();
-    day6::run();
-    day7::run();
+    let results = [
+        day1::run,
+        day2::run,
+        day3::run,
+        day4::run,
+        day5::run,
+        day6::run,
+        day7::run,
+    ]
+    .into_par_iter()
+    .map(|s| s())
+    .collect::<Vec<_>>();
+    for result in results {
+        println!("{result}");
+    }
+
     // gen run
     let time = start.elapsed();
     println!("Total time: {time:?}");
@@ -48,7 +68,7 @@ fn main() {
 #[macro_export]
 macro_rules! bp {
     ($n:expr) => {
-        pub fn run() {
+        pub fn run() -> String {
             let input = if crate::TEST_MODE.load(std::sync::atomic::Ordering::SeqCst) {
                 include_str!(concat!(
                     concat!(concat!("input/day"), stringify!($n)),
@@ -62,7 +82,10 @@ macro_rules! bp {
             };
 
             let start = std::time::Instant::now();
-            let result = main(input);
+            let result = (0..crate::BATCH_COUNT.load(std::sync::atomic::Ordering::SeqCst))
+                .map(|_| main(input))
+                .last()
+                .expect("Batch count of 0 is not allowed");
             let time = start.elapsed();
 
             crate::Results::print(&result, $n, time)
@@ -81,14 +104,14 @@ macro_rules! match_unwrap {
 }
 
 trait Results {
-    fn print(&self, day: u8, time: Duration);
+    fn print(&self, day: u8, time: Duration) -> String;
 }
 
 impl<T: Display, U: Display> Results for (T, U) {
-    fn print(&self, day: u8, time: Duration) {
-        println!(
+    fn print(&self, day: u8, time: Duration) -> String {
+        format!(
             "Day {day} results:\nPart 1: {}\nPart 2: {}\nTime: {time:?}\n",
             self.0, self.1
-        );
+        )
     }
 }
